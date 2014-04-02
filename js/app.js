a7f6255f778d093e2c29c4206eaccfe2e7f477b7
Chris Lindgren <chris.a.lindgren@gmail.com>
2014-04-02 13:58:42 -0500

    revised app.js to utilize FixtureAdapter/Model-Data approach

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 //need to tell our templates where to render on the page
 App.Router.map(function() {
-  /*
+  /*================================================
     URL   == http://example.com#/about
     Path  == /about
     Route == about
     Finds {{outlet}} expression, then inserts route
-  */
+    ==============================================*/
   this.route('about');
-
-  /*
-    Resource routes differ from plain routes
-    1) 
-    2)
-  */
   this.resource('unicorns', function() {
-    /*
+    /*==============================================
       -nested resource route w/ dynamic segment path
       -Ember path: unicorns/:name
       -Route: unicorn
       -URL: http://example.com#/unicorns/Shimmer
-    */
-    this.resource('unicorn', { path: '/:name' })
+      ============================================*/
+    this.resource('unicorn', { path: '/:unicorn_id' })
   });
 });
 
@@ -44,35 +42,55 @@ App.UnicornsArrayController = Ember.Controller.extend({
   }.property()
 });
 
-//Unicorn route with a basic model property
-//model can be obj or array of objs
-//Fetches the model to st on the controller
+/*=============================================
+  Unicorn route with a basic model property
+  model can be obj or array of objs
+  Finds/fetches all data from fixture adapter 
+  to set onto the model to set on the controller
+  =============================================*/
 App.UnicornsRoute = Ember.Route.extend({
   model: function() {
-    return App.UNICORNS;
+    return this.store.findAll('unicorn');
   }
 });
 
+/*=============================================
+  NOTE: This is actually Ember's default behavior,
+  so I could delete this code, if all I want this
+  Route to perform is find the unique_id
+  =============================================*/
 App.UnicornRoute = Ember.Route.extend({
   model: function(path) {
-    return App.UNICORNS.findBy('name', path.name);
+    return this.store.find('unicorn', path.unicorn_id);
   }
 });
 
-App.UNICORNS = [
+/*good practice == defining data types,
+    otherwise Ember will make assumptions.*/
+App.Unicorn = DS.Model.extend({
+  name: DS.attr('string'),
+  age: DS.attr('number'),
+  bio: DS.attr('string'),
+  img: DS.attr('string')
+});
+
+App.Unicorn.FIXTURES = [
   {
+    id: 1,
     name: 'Pilgrim',
     age: 854,
     bio: 'Pilgrim is a young unicorn, but the most bold.',
     img: 'assets/img/pilgrim.jpg'
   },
   {
+    id: 2,
     name: 'Shimmer',
     age: 5000,
     bio: 'Shimmer shines, and she is the unicorn leader.',
     img: 'assets/img/shimmer.jpg'
   },
   {
+    id: 3,
     name: 'Fauxnie',
     age: 2345,
     bio: 'Fauxnie lurks in the shadow arts of hipster.',
